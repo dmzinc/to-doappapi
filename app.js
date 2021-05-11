ca85d15d36a81efc94d06c1f796cd61c5ff7f06b
forbes <afolabi2245@gmail.com>
2021-05-11 21:27:57 +0100

    nodemon and morgan installed

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const app = express();
 
+const morgan = require("morgan");
+
 const listsRoutes = require("./api/routes/lists");
 
+//Routes which handles requests
 app.use("/lists", listsRoutes);
 
 module.exports = app;
