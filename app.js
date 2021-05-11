d1e0c03729a4805a26229cb8c37bb3987059da35
forbes <afolabi2245@gmail.com>
2021-05-11 21:54:48 +0100

    update

--- app.js
+++ app.js
@@ -4,9 +4,28 @@ const app = express();
 
 const morgan = require("morgan");
 
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const listsRoutes = require("./api/routes/lists");
 
 //Routes which handles requests
 app.use("/lists", listsRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
