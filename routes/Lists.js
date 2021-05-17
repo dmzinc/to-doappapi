37309aa8c237fc93898fcd52f26085a209914736
forbes <afolabi2245@gmail.com>
2021-05-17 17:16:42 +0100

    done

--- routes/Lists.js
+++ routes/Lists.js
@@ -4,14 +4,14 @@ const router = express.Router();
 
 const List = require("../models/Lists");
 
-//Get all routes
+//Get All Routes
 router.get("/", async (req, res) => {
   const lists = await List.find();
 
   res.json(lists);
 });
 
-//Create new list
+//Create New List
 router.post("/new", async (req, res) => {
   const newList = new List(req.body);
 
@@ -20,7 +20,7 @@ router.post("/new", async (req, res) => {
   res.json(savedList);
 });
 
-//Get a specific list
+//Get a Specific List
 router.get("/get/:id", async (req, res) => {
   const q = await List.findById({ _id: req.params.id });
 
@@ -32,6 +32,20 @@ router.delete("/delete/:id", async (req, res) => {
   const result = await List.findByIdAndDelete({ _id: req.params.id });
 
   res.json(result);
+});
+
+//Update List
+router.patch("/update/:id", async (req, res) => {
+  const q = await List.updateOne({ _id: req.params.id }, { $set: req.body });
+  res.json(q);
+});
+
+//Get Random Lists
+router.get("/random", async (req, res) => {
+  const count = await List.countDocuments();
+  const random = Math.floor(Math.random() * count);
+  const q = await List.findOne().skip(random);
+  res.json(q);
 });
 
 module.exports = router;
